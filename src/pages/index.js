1d95dd5e9feda49ad8f1eb7bd189dc55bbd63e95
Manny Munoz <em66@uw.edu>
2021-02-16 15:55:50 -0800

    Added reusme page layout and SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from "react";
+import '../../resume.css';
+import SEO from "../components/seo";
+import Bio from '../components/Bio';
+import Projects from '../components/Projects';
+import Experiences from '../components/Experiences';
+import Tech from '../components/Tech';
+import Contact from '../components/Contact';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+   <div className="d-flex justify-content-center flex-column">
+      <SEO title="Portflio"></SEO>
+      <header>
+         <h1 className="text-left pl-3">Emmanuel "Manny" Munoz</h1>
+         <hr />
+      </header>
 
-export default IndexPage
+      <main>
+
+         <Bio />
+
+         <Projects />
+
+         <Experiences />
+
+         <Tech />
+
+         <Contact />
+      </main>
+   </div>
+);
+
+export default IndexPage;
