4d4c0f562b358a925e6ca5be67cd94d157d8cebe
Manny Munoz <em66@uw.edu>
2021-02-16 15:54:15 -0800

    Added component to disply bio

---
+++ src/components/Bio.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Bio = () => {
+   return (
+      <div className="container mb-5" id="bio">
+         <h2 className="text-center">
+            "Excellence isn't an art, it's a way of life. <br /> We are what we repeatedly do." - Furi&trade;
+         </h2>
+         <div>
+            <h3 className="mb-3">Bio</h3>
+            <div className="col-sm">
+               <p className="regtext">
+                  Hello! My name is Emmanuel Munoz but I go by Manny. I was born and raised in 
+                  Tacoma, WA by two amazing Colombian immigrants. I am a Computer Science and 
+                  Systems student at the University of Washington, Tacoma campus with a passion
+                  for education! I am a member of both the latino and gay communities and proud
+                  of it! After college, I hope to start my own company in my hometown and support
+                  my community. The quote at the top is my personal mantra in life.
+               </p>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Bio;
