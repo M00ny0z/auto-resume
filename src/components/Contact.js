70817579c31b2a77ad125a15cd630c139d45977a
Manny Munoz <em66@uw.edu>
2021-02-16 15:54:33 -0800

    Added component to display contact info

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Contact = () => {
+   return (
+      <div className="container">
+         <h3 className="text-center">Contact Info</h3>
+         <ul className="list-inline d-flex justify-content-center">
+            <li className="list-inline-item">
+               <a href="https://github.com/M00ny0z?tab=repositories">
+                  <i className="contact fab fa-github fa-5x"></i>
+               </a>
+            </li>
+            <li className="list-inline-item">
+               <a href="mailto:1mannymunoz@gmail.com">
+                  <i className="contact far fa-envelope fa-5x"></i>
+               </a>
+            </li>
+            <li className="list-inline-item">
+               <a href="https://www.linkedin.com/in/m00ny0z/">
+                  <i class="contact fab fa-linkedin fa-5x"></i>
+               </a>
+            </li>
+         </ul>
+      </div>
+   );
+};
+
+export default Contact;
