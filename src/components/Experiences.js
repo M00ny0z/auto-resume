dc2774db442d123c9996769999cdc591a524713a
Manny Munoz <em66@uw.edu>
2021-02-16 15:54:49 -0800

    Added component to display previous dev experiences

---
+++ src/components/Experiences.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import Tech from './Tech';
+
+const Experiences = () => {
+   return (
+      <div className="container mb-5" id="experiences">
+            <h2 className="mb-3">Previous Experiences</h2>
+            <div className="d-flex flex-row flex-wrap justify-content-around">
+
+               <div className="card mb-4 bg-exp" style={{width: '18rem'}}>
+                  <div className="card-body">
+                     <div className="d-flex flex-column justify-content-between">
+                        <h5 className="card-title">Software Engineer Intern</h5>
+                        <h6 className="card-subtitle mb-3 border-bottom border-white border-5">Oracle Cloud Infrastructure</h6>
+                     </div>
+                     <p className="card-text">
+                        Developed infrastructure (Scripts/Websites) for the class as necessary.
+                        Lead a bi-weekly class going in-depth into lecture material.
+                        Helped create new material for the class as necessary.
+                     </p>
+                  </div>
+               </div>
+
+               <div className="card mb-4 bg-exp" style={{width: '18rem'}}>
+                  <div className="card-body">
+                     <div className="d-flex flex-column justify-content-between">
+                        <h5 className="card-title">Software Engineer Intern</h5>
+                        <h6 className="card-subtitle mb-3 border-bottom border-white border-5">Namatad Inc.</h6>
+                     </div>
+                     <p className="card-text">
+                        Developed a marketing site using Gatsby to showcase company information 
+                        and a Content-Management-Site (CMS) using React for non-developers to 
+                        easily update marketing information.
+                     </p>
+
+                     <p className="card-text">
+                        Desgined and implemented a REST API to service an in-house CMS to update 
+                        marketing site.
+                     </p>
+
+                     <p className="card-text">
+                        Build and developed overall project infrastructure utilizing AWS Lambda, 
+                        Amplify, and S3 focusing on low-cost and auto-scaling.
+                     </p>
+                  </div>
+               </div>
+
+               <div className="card mb-4 bg-exp" style={{width: '18rem'}}>
+                  <div className="card-body">
+                     <div className="d-flex flex-column justify-content-between">
+                        <h5 className="card-title">Teacher's Assistant (CSE154)</h5>
+                        <h6 className="card-subtitle mb-3 border-bottom border-white border-5">
+                           Paul G. Allen School of Computer Science and Engineering
+                        </h6>
+                     </div>
+                     <p className="card-text">
+                        Lead a bi-weekly class of ~15 students going in-depth into lecture material.
+                     </p>
+                     <p className='card-text'>
+                        Helped create new class material for the transition to teach new back-end 
+                        language.
+                     </p>
+                  </div>
+               </div>
+
+               <div className="card mb-4 bg-exp" style={{width: '18rem'}}>
+                  <div className="card-body">
+                     <div className="d-flex flex-column justify-content-between mb-3">
+                        <h5 className="card-title">Web Developer (Full-Stack)</h5>
+                        <h6 className="card-subtitle mb-3 border-bottom border-white border-5">
+                           Student Life Department (UWT)
+                        </h6>
+                     </div>
+                     <p className="card-text">
+                        Created a full-stack web application that allows students to see currently 
+                        available resources.
+                     </p>
+                     <p className="card-text">
+                        Created a Content Management System that allows advisors
+                        to update the site without writing code. Both built with HTML/CS/JS for the 
+                        front-end and PHP/SQL for the back-end.
+                     </p>
+                  </div>
+               </div>
+
+               <div className="card mb-4 bg-exp" style={{width: '18rem'}}>
+                  <div className="card-body">
+                     <h5 className="card-title">CS Educator/Mentor</h5>
+                     <h6 className="card-subtitle mb-3 border-bottom border-white border-5">
+                        Neighborhood House
+                     </h6>
+                     <p className="card-text">
+                        Mentored youth and tought basic CS principles through robotics
+                     </p>
+                  </div>
+               </div>
+
+               <div className="card mb-4 bg-exp" style={{width: '18rem'}}>
+                  <div className="card-body">
+                     <h5 className="card-title">Student Advisory Board Member</h5>
+                     <h6 className="card-subtitle mb-3 border-bottom border-white border-5">
+                        Housing & Food Services (UW)
+                     </h6>
+                     <p className="card-text">
+                        Identify student concerns with the university food system. 
+                     </p>
+                     <p className="card-text">
+                        Collaborated with administration to create viable solutions to identified 
+                        concerns.
+                     </p>
+                  </div>
+               </div>
+         </div>
+      </div>
+   );
+};
+
+export default Experiences;
